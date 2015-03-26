e13baa297da84edd74dcab9404a132dac0117e4b
bvigar <bvigar91@gmail.com>
2015-03-26 13:32:40 -0500

    Importing project skeleton.

---
+++ src/main/java/com/github/bvigar/eclipse/osgi/OsgiBundleUtilityApplication.java
@@ -0,0 +1,15 @@
+package com.github.bvigar.eclipse.osgi;
+
+/**
+ * Description.
+ * @author Trent Vigar
+ */
+public class OsgiBundleUtilityApplication {
+
+    /**
+     * @param args
+     */
+    public static void main(final String[] args) {
+        // TODO
+    }
+}
